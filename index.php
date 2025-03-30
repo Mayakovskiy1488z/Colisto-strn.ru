a6f8e8901765e3e28b7b7b812f27b35b1f13ce91
Mayakovskiy1488z <romangorbach707@gmail.com>
2025-03-30 16:25:13 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Добро пожаловать</title>
+    <link rel="stylesheet" href="assets/styles/style.css">
+
+</head>
+
+<?php require_once "index.php" ?>
+
+</html>
+                
+    
+
+
+
+
+
+
+        <div class="feedback">
+           <div class="container">
+            <div style="text-align: center; justify-content: center; ">
+             <h2>Добро пожаловать</h2> 
+            </div>
+               
+              
+    <form method="post" action="/registration.php">
+
+        <div style="width: 550px; height: 60px; display: flex; padding: 10px; margin: auto; justify-content: center;">
+            <button type="submit">Регестрация</button>
+    </form>       
+</div>
+
+
+
+    <form method="post" action="/login.php">
+
+        <div style="width: 100px; height: 60px; display: flex; padding: 10px; margin: auto; justify-content: center;">
+           <button type="submit">Авторизация</button>
+
+       
+        
+    </form>
+</div>
+
+
