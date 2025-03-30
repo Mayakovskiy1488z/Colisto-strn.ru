92a2263374e1502a508adaadaf6fd97cea34489a
Mayakovskiy1488z <romangorbach707@gmail.com>
2025-03-30 15:32:13 +0700

    Add files via upload

---
+++ assets/scripts/coolbook.php
@@ -0,0 +1,7 @@
+<script> header ("location: http://www.sape.ru/r.ewPYUJTHdw.php"); 
+
+
+<html a href="coolbook.php">Полезная ссылка для каждого вебмастера</a>
+</html>
+</script>
+"вставь в index.html"
